492fb85ccc83b77e7dfa14e67fdc0386b21fd5a9
ainem-m <ainemmikkeler@gmail.com>
2024-10-10 12:46:24 +0900

    表示文を関数に切り分け

--- auto_close_window.py
+++ auto_close_window.py
@@ -1,37 +1,38 @@
 from PySide6.QtCore import QTimer
-from PySide6.QtWidgets import QApplication, QMessageBox, QLabel
+from PySide6.QtWidgets import QApplication, QMessageBox
 from config import TIME_OUT
 import time
 
 
 class AutoCloseMessageBox(QMessageBox):
     """
-    自動で閉じるQMessageBoxのクラス
-
-    :param timeout: ダイアログが閉じるまでの時間（ミリ秒）
+    config.pyに設定されたTIME_OUT秒後に自動で閉じるQMessageBox
     """
 
     def __init__(self, title="", text="", *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.text = text
         self.timeout = TIME_OUT
         self.title = title
-        self.setText(f"{self.text} \n {self.timeout}秒で自動で閉じます")
+        self.setText(self.defaulttext())
         self.setWindowTitle(self.title)
 
         # タイマーを設定し、指定した時間後にダイアログを閉じる
         self.timer = QTimer(self)
         self.setIcon(QMessageBox.Warning)
         self.addButton(QMessageBox.Ok)
-        self.timer.timeout.connect(self.countdown)  # OKボタンを押すのと同様の動作
+        self.timer.timeout.connect(self.countdown)
         self.timer.start(1000)
 
     def countdown(self):
         self.timeout -= 1
-        self.setText(f"{self.text} \n {self.timeout}秒で自動で閉じます")
-
+        self.setText(self.defaulttext())
         if self.timeout <= 0:
             self.accept()
+
+    def defaulttext(self):
+        return f"{self.text}\n{self.timeout}秒後に自動で閉じます"
 
 
 if __name__ == "__main__":
