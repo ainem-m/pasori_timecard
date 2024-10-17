1cbc3a968cc8c4648831c5b5c25192d1c95671f3
ainem-m <ainemmikkeler@gmail.com>
2024-10-17 09:58:07 +0900

    不要なimportを削除

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
 from loguru import logger
 from config import LOG_FILE_PATH
-import sys
 
 logger.add(LOG_FILE_PATH, level="TRACE", rotation="10 MB")
 
