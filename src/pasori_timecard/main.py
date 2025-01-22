acdb6b76a030af5229a02404ebd729eff3da5cd1
ainem-m <ainemmikkeler@gmail.com>
2025-01-22 13:13:07 +0900

    フォルダ構成を変更（２回目のトライ）

--- src/pasori_timecard/main.py
+++ src/pasori_timecard/main.py
@@ -3,7 +3,9 @@ from main_window import MainWindow
 from loguru import logger
 import config
 from pathlib import Path
-from auto_close_window import AutoCloseMessageBox
+import sys
+
+sys.path.append("src")
 
 logger.add(config.LOG_FILE_PATH, level="TRACE", rotation="10 MB")
 
